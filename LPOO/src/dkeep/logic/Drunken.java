dfb323c49389ce7ce2578193b599dd27c629cd9c
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-03-08 16:37:42 +0000

    Pass

--- LPOO/src/dkeep/logic/Drunken.java
+++ LPOO/src/dkeep/logic/Drunken.java
@@ -6,7 +6,7 @@ public class Drunken extends Guard {
 	private int backwards;
 	private int sleep;
 	private char sSymbol;
-	
+
 	public Drunken(int[] pos, char[] path, char symbol, char sSymbol) {
 		super(pos, path, symbol);
 		this.backwards = 0;
@@ -16,9 +16,9 @@ public class Drunken extends Guard {
 
 	@Override
 	public char getSymbol() {
-		if(sleep == 1)
+		if (sleep == 1)
 			return sSymbol;
-		else 
+		else
 			return super.getSymbol();
 	}
 
@@ -27,64 +27,63 @@ public class Drunken extends Guard {
 		Random generator = new Random();
 		int newSleep = generator.nextInt(2);
 
-		if(newSleep == 1)
-		{
+		if (newSleep == 1) {
 			sleep = 1;
 			return;
 		}
 
-		if(sleep == 1 && newSleep == 0)
-		{
+		if (sleep == 1 && newSleep == 0) {
 			sleep = 0;
 			this.backwards = generator.nextInt(2);
 		}
 
 		char command = this.getPath()[this.getCounter()];
-		if(backwards == 1)
-		{
+		if (backwards == 1) {
 			switch (command) {
 			case 's':
-				if(map[this.getPos()[0] - 1][this.getPos()[1]] == ' ' )
-				{
+				if (map[this.getPos()[0] - 1][this.getPos()[1]] == ' ') {
 					this.getPos()[0]--;
 					this.setCounter(this.getCounter() - 1);
 				}
 				break;
 
 			case 'd':
-				if(map[this.getPos()[0]][this.getPos()[1] - 1] == ' ')
-				{
+				if (map[this.getPos()[0]][this.getPos()[1] - 1] == ' ') {
 					this.getPos()[1]--;
 					this.setCounter(this.getCounter() - 1);
 				}
 				break;
-					
+
 			case 'w':
-				if(map[this.getPos()[0] + 1][this.getPos()[1]] == ' ')
-				{
+				if (map[this.getPos()[0] + 1][this.getPos()[1]] == ' ') {
 					this.getPos()[0]++;
 					this.setCounter(this.getCounter() - 1);
 				}
 				break;
 
 			case 'a':
-				if(map[this.getPos()[0]][this.getPos()[1] + 1] == ' ')
-				{
+				if (map[this.getPos()[0]][this.getPos()[1] + 1] == ' ') {
 					this.getPos()[1]++;
 					this.setCounter(this.getCounter() - 1);
 				}
 				break;
-				
+
 			default:
 				break;
 			}
-			
-		}
-		else
+
+		} else
 			super.move(map);
-		if(this.getCounter() < 0)
+		if (this.getCounter() < 0)
 			this.setCounter(this.getCounter() + this.getPath().length);
 		this.setCounter(this.getCounter() % this.getPath().length);
+	}
+
+	@Override
+	public boolean killedHero(Hero hero) {
+		if (sleep == 1)
+			return false;
+		return super.killedHero(hero);
 	}
 
 }
