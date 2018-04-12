7954280b717213134c419d8adf850ddff6cfbf3b
Antero Campos Gandra <up201607926@fe.up.pt>
2018-04-12 17:04:03 +0100

    Lift started

---
+++ Lift/android/src/com/lift/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lift.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lift.game.LiftGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LiftGame(), config);
+	}
+}
